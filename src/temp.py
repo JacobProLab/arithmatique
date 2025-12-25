5b6bf7565d58a14e31f76c1034f9e337c14883e4
JacobProLab <jacob.provencher.science@gmail.com>
2025-12-25 14:01:46 -0500

    Worked on back-end + implement models for movies app

--- src/temp.py
+++ src/temp.py
@@ -1,5 +1,11 @@
+import sys
 import random
-from collections.abc import Iterator
+import logging
+
+
+logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=logging.INFO)
+logger = logging.getLogger()
+logger.disabled = True
 
 class Arithmatique:
 
@@ -9,10 +15,13 @@ class Arithmatique:
         self.lower_bound: int = lower_bound
         self.upper_bound: int = upper_bound
 
-    def _get_expression(self) -> str:
+    def _get_expression(self) -> tuple:
 
+        # Retrieve random variables' values
         n1 = random.randint(self.lower_bound, self.upper_bound)
-        n2 = random.randint(self.lower_bound, self.upper_bound)
+        n2 = 0
+        while n2 == 0:
+            n2 = random.randint(self.lower_bound, self.upper_bound)
         op = random.choice(["add", "sub", "mul", "truediv"])
 
         op_lut = {"add": (n1.__add__(n2), "+"),
@@ -23,18 +32,36 @@ class Arithmatique:
         operator = op_lut[op][1]
         equal = op_lut[op][0]
 
-        return f"{n1} {operator} {n2} = {equal}"
-    
-    def _expression_generator(self) -> Iterator[str]:
-        for i in range(self.n):
-            yield self._get_expression()
+        # Assert only whole number results are given if operator == truediv
+        if equal % 1:
+            while equal % 1:
+                n2 = 0
+                while n2 == 0:
+                    n2 = random.randint(self.lower_bound, self.upper_bound)
+                equal = n1.__truediv__(n2)
+
+        return (n1, operator, n2, int(equal))
     
     def __str__(self) -> str:
-        return "\n".join(f"{i}) {expr}" for i, expr in enumerate(self._expression_generator(), start=1))
+
+        expressions = [self._get_expression() for i in range(self.n)]
+
+        max_i = len(str(self.n))
+        max_a = max(len(str(values[0])) for values in expressions)
+        max_b = max(len(str(values[2])) for values in expressions)
+        max_e = max(len(str(values[-1])) for values in expressions)
+        
+        result = []
+        for i, values in enumerate(expressions, start=1):
+            string = "{:>{i}}) {:>{a}}{:^3}{:<{b}} = {:>{e}}".format(i, *values, i=max_i, a=max_a, b=max_b, e=max_e)
+            result.append(string)
+
+        return "\n".join(expr for expr in result)
 
 
-session = Arithmatique(10, 0, 30)
-print(session)
+if __name__ == "__main__":
+    session = Arithmatique(10, 0, 50)
+    print(session)
 
 
 
